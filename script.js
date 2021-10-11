121f95579bfffa1439d533942544e2030f05851b
Luis Escobar <lae311@stern.nyu.edu>
2021-10-11 07:28:32 -0400

    finalied mouseovers

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-
-function SetNewImage () {
+function SetNewImage1 () {
     document.getElementById("me").src= "/Users/administrator/Development/Code/learn-prework/website2/img/Logo.png";
 }
 
-function SetOldImage () {
-    document.getElementById("me").src= "/Users/administrator/Development/Code/learn-prework/website2/img/Me.png"
+function SetOldImage1 () {
+    document.getElementById("me").src= "/Users/administrator/Development/Code/learn-prework/website2/img/Me.png";
+}
+
+function SetNewImage2 () {
+    document.getElementById("envir").src= "/Users/administrator/Development/Code/learn-prework/website2/img/sunnyside-alexanderthomspon_3f__large.jpeg";
+}
+
+function SetOldImage2 () {
+    document.getElementById("envir").src= "/Users/administrator/Development/Code/learn-prework/website2/img/Flag_of_Colombia.png";
+}
+
+function SetNewImage3 () {
+    document.getElementById("feel").src= "/Users/administrator/Development/Code/learn-prework/website2/img/better.jpeg";
+}
+
+function SetOldImage3 () {
+    document.getElementById("feel").src= "/Users/administrator/Development/Code/learn-prework/website2/img/suck.png";
 }
