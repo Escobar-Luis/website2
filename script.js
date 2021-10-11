78b3e23ab4d12fb19b71dd45bc2cb6f64835e748
Luis Escobar <lae311@stern.nyu.edu>
2021-10-11 06:51:34 -0400

    Added mouseover function and added IDs

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+function SetNewImage () {
+    document.getElementById("me").src= "/Users/administrator/Development/Code/learn-prework/website2/img/Logo.png";
+}
+
+function SetOldImage () {
+    document.getElementById("me").src= "/Users/administrator/Development/Code/learn-prework/website2/img/Me.png"
+}
